65ec9da771bb6f50bd2be42c2948dbe621976fd5
gamejoxx <160375716+gamejoxx@users.noreply.github.com>
2024-03-29 03:07:40 -0700

    added controls test

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,10 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] Rigidbody2D PlayerRigidBody;
+
+    //variables for walking
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+    [SerializeField] float fallMultiplier = 2.5f;
+    [SerializeField] float lowJumpMultiplier = 2f;
+    [SerializeField] float groundCheckRadius;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform groundCheck;
+
+    [SerializeField] bool isGrounded;
+    [SerializeField] bool isJumping;
+    [SerializeField] bool isGround;
+    [SerializeField] bool isFalling;
 
 
     // Start is called before the first frame update
